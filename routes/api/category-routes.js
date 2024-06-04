10b1cfec80da7a03a9697c41786f8d1b4fd4646f
TimWong <timwong.tw93@gmail.com>
2024-06-04 15:21:18 +1000

    fixed typo in category routes and added put requests for category and tag routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -23,7 +23,7 @@ router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   try {
-    const catData = await Category.findbyPk(req.params.id, {
+    const catData = await Category.findByPk(req.params.id, {
       include: [{model: Product}],
     });
 
@@ -43,7 +43,9 @@ router.post('/', async (req, res) => {
   // create a new category
   try {
     const catData = await Category.create(req.body);
+
     res.status(200).json(catData);
+
   } catch (error) {
     res.status(500).json(error);
   }
@@ -51,6 +53,23 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const catData = await Category.update(req.body, {
+      where:  {
+        id: req.params.id,
+      }
+    });
+
+    if(!catData) {
+      res.status(404).json({message: `No category found with this ID.`});
+      return;
+    }
+
+    res.status(200).json(catData);
+
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -42,7 +42,9 @@ router.post('/', async (req, res) => {
   // create a new tag
   try {
     const tagData = await Category.create(req.body);
+
     res.status(200).json(tagData);
+
   } catch (error) {
     res.status(500).json(error);
   }
@@ -50,6 +52,23 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagData = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      }
+    });
+
+    if(!tagData) {
+      res.status(404).json({message: `No tag found with this ID.`});
+      return;
+    }
+
+    res.status(200).json(tagData);
+    
+  } catch (error) {
+    
+  }
 });
 
 router.delete('/:id', async (req, res) => {
